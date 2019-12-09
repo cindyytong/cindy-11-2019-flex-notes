4b2bcdd73783ac0e10ad9ef181a1f0b17872284b
cindytong <tongcindyy@gmail.com>
2019-12-09 18:07:46 -0500

    week4day1 notes

---
+++ week4_day1_circle_notes.rb
@@ -0,0 +1,27 @@
+######### Week 4 Day 1 #########
+
+################################## 
+#Agenda:
+################################## 
+# 1. Retro on assessment 
+# 2. Go through git workflow for notes repo
+# https://github.com/cindyytong/cindy-11-2019-flex-notes.git
+# 3. Knights Travails - trace path review 
+# 4. Tic tac toe 
+    # a. How to walk through code 
+    # b. winning_node and losing_node 
+
+################################## 
+#1. Assessment Tips 
+################################## 
+# Make use of resources provided 
+# Do not cram during the weekend
+# Look at the learning objectives and not memorize questions so you understand concepts  
+# Don't wait for last minute 
+# Leitner method: make slides of examples of code to go back to
+# Take a break, self care! 
+# Sleep the day before
+# Use scratch paper 
+# Go through use cases before starting to code 
+# Explain things in own words to learn hard concepts
+
